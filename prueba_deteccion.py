8aff099f1e886055a59e104cea62a28e0ce53947
Rodrigo <rodrigo-morales2011@hotmail.es>
2020-09-11 10:58:15 -0400

    se agrego la funcion de calcular el tiempo promedio de espera

--- prueba_deteccion.py
+++ prueba_deteccion.py
@@ -1,10 +1,10 @@
 import sched, time
 import object_tracker_modificado
 s = sched.scheduler(time.time, time.sleep)
-global max_person_counter
+global MAXIMUM_NUMBER_OF_PEOPLE_DETECTED
 def preguntar(sc):
        cant_person = 1
-       print(max_person_counter)
+       print(MAXIMUM_NUMBER_OF_PEOPLE_DETECTED)
        if object_tracker_modificado.track.cant_actual == cant_person or object_tracker_modificado.cant_actual > cant_person:
               print(10+5)
               object_tracker_modificado.max_person_counter = 0
